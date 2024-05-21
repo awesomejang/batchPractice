7aaec0453847ab40e52128b6e31c1e270ebf7ab8
jangsangyun <uljang1995@gmai.com>
2024-05-21 23:56:58 +0900

    번호 생성 및 jobParameter 추가
    1. jobParameter추가로 각 job 분기처리

--- src/main/java/study/batchpractice/Tasks/CreateLottoNumber.java
+++ src/main/java/study/batchpractice/Tasks/CreateLottoNumber.java
@@ -1,7 +1,31 @@
 package study.batchpractice.Tasks;
 
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 // 한게임에 해당하는 로또 번호 6개의 중복되지 않은 숫자를 생성한다. (1~45)
-public class CreateLottoNumber {
+public class CreateLottoNumber implements Tasklet {
 
+    @Override
+    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
 
+        return null;
+    }
+
+    public String createLottoNumbers() {
+        HashSet<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+        while (numbers.size() < 6) {
+            // 0 ~ ...
+            int number = random.nextInt(45) + 1;
+            numbers.add(number);
+        }
+        return numbers.stream().map(String::valueOf).collect(Collectors.joining(","));
+    }
 }
--- src/test/java/study/batchpractice/LottoTest.java
+++ src/test/java/study/batchpractice/LottoTest.java
@@ -1,5 +1,6 @@
 package study.batchpractice;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,14 +10,15 @@ import study.batchpractice.Tasks.LottoCountCheckTasklet;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
+import java.util.HashSet;
 import java.util.Random;
 
 @Transactional
-@SpringBootTest
+//@SpringBootTest
 public class LottoTest {
 
-    @Autowired
-    private LottoCountCheckTasklet lottoCountCheckTasklet;
+//    @Autowired
+//    private LottoCountCheckTasklet lottoCountCheckTasklet;
 
     @Test
     public void makeSaturdayDate() {
@@ -26,8 +28,14 @@ public class LottoTest {
 
     @Test
     public void createLottoNumbers() {
+        HashSet<Integer> numbers = new HashSet<>();
         Random random = new Random();
-        int i = random.nextInt(12);
-        System.out.println("i = " + i);
+        while (numbers.size() < 6) {
+            // 0 ~ ...
+            int number = random.nextInt(45) + 1;
+            numbers.add(number);
+        }
+        Assertions.assertThat(numbers.size()).isEqualTo(6);
+        Assertions.assertThat(numbers).allMatch(number -> number >= 1 && number <= 45);
     }
 }
