81c8b3f75039dcc6249ba3b43861525092a66041
jangsangyun <uljang1995@gmai.com>
2024-06-08 13:01:21 +0900

    ItemProcessor 초기작업

--- src/main/java/study/batchpractice/processor/LottoItemProcessor.java
+++ src/main/java/study/batchpractice/processor/LottoItemProcessor.java
@@ -1,8 +1,20 @@
 package study.batchpractice.processor;
 
+import study.batchpractice.entities.LottoEntity;
+import study.batchpractice.entities.TotalLottoEntity;
+
 import javax.batch.api.chunk.ItemProcessor;
 
-public class LottoItemProcessor implements ItemProcessor {
+public class LottoItemProcessor implements ItemProcessor<LottoEntity, TotalLottoEntity> {
+
+    @Override
+    public TotalLottoEntity processItem(LottoEntity item) {
+        return TotalLottoEntity.builder()
+                .targetDate(item.getTargetDate())
+                .totalAmount(item.getTotalAmount())
+                .totalCount(item.getTotalCount())
+                .build();
+    }
 
     @Override
     public Object processItem(Object o) throws Exception {
