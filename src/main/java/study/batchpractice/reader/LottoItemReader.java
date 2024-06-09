9bf87b87e6f5b648c7a42bc22dfcbb89134e55c9
jangsangyun <uljang1995@gmai.com>
2024-06-09 23:56:30 +0900

    ItemProcessor LottoRepository 추가

--- src/main/java/study/batchpractice/reader/LottoItemReader.java
+++ src/main/java/study/batchpractice/reader/LottoItemReader.java
@@ -1,30 +1,58 @@
 package study.batchpractice.reader;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import study.batchpractice.entities.LottoEntity;
+import study.batchpractice.repository.LottoRepository;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
-public class LottoItemReader {
-    private final EntityManagerFactory entityManagerFactory;
+//@RequiredArgsConstructor
+public class LottoItemReader implements ItemReader<List<LottoEntity>> {
 
-    public LottoItemReader(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
+    private final EntityManager entityManager;
+    private final LottoRepository lottoRepository;
+    private final LocalDate localDate;
+
+    public LottoItemReader(EntityManager entityManager, LottoRepository lottoRepository, LocalDate localDate) {
+        this.entityManager = entityManager;
+        this.lottoRepository = lottoRepository;
+        this.localDate = localDate;
     }
 
-    public JpaCursorItemReader<LottoEntity> build(LocalDate targetDate) {
-        return new JpaCursorItemReaderBuilder<LottoEntity>()
-                .name("lottoItemReader")
-                .entityManagerFactory(entityManagerFactory)
-                .queryString("SELECT le FROM LottoEntity le where le.targetDate = :targetDate")
-                .parameterValues(Map.of("targetDate", targetDate))
-                .build();
+    @Override
+    public List<LottoEntity> read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+//        lottoRepository.find
+        return List.of();
     }
+
+
+//    private final EntityManagerFactory entityManagerFactory;
+//
+//    public LottoItemReader(EntityManagerFactory entityManagerFactory) {
+//        this.entityManagerFactory = entityManagerFactory;
+//    }
+//
+//    public JpaCursorItemReader<LottoEntity> build(LocalDate targetDate) {
+//        return new JpaCursorItemReaderBuilder<LottoEntity>()
+//                .name("lottoItemReader")
+//                .entityManagerFactory(entityManagerFactory)
+//                .queryString("SELECT le FROM LottoEntity le where le.targetDate = :targetDate")
+//                .parameterValues(Map.of("targetDate", targetDate))
+//                .build();
+//    }
 }
